8da670e5e0560c967fe63b625c486e3368091eb8
JawzoD3TH <hassimjawid@gmail.com>
2025-04-26 23:41:15 +0200

    No Changes Made

--- NumberExtensions.cs
+++ NumberExtensions.cs
@@ -20,7 +20,7 @@ public static class NumberExtensions
 
         }
         catch { }
-
+        
         return source.AsParallel().WithDegreeOfParallelism(AmountOfThreads()).ElementAt(index);
     }
 
